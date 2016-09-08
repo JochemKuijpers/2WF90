6ab7c847b25c8b589b6dda088da0ef324af1b896
Stufingo <geniekort@hotmail.com>
2016-09-08 11:38:46 +0200

    Updated Calculator

---
+++ src/main/Calculator.java
@@ -0,0 +1,56 @@
+package main;
+
+public class Calculator {
+
+	private int add, sub, mul; 
+	
+	public Calculator(){
+		add = sub = mul = 0;
+	}
+	
+	
+	/**
+	 * 
+	 * @param n1 Number 1 to be added
+	 * @param n2 Number 2 to be added
+	 * @return Input numbers added together
+	 */
+	public Number add(Number n1, Number n2){
+		
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param n1
+	 * @param n2
+	 * @return The subtraction n1-n2
+	 */
+	public Number subtract(Number n1, Number n2){
+		
+		return null;
+	}
+	
+	/**
+	 * Calculates the product of the input numbers using the primary school method
+	 * @param n1
+	 * @param n2
+	 * @return The product of n1 and n2
+	 */
+	public Number mulPrimarySchool(Number n1, Number n2){
+		
+		return null;
+	}
+	
+	/**
+	 * Calculates the product of the input numbers using the karatsuba method
+	 * @param n1
+	 * @param n2
+	 * @return The product of n1 and n2
+	 */
+	public Number mulKaratsuba(Number n1, Number n2){
+		
+		return null;
+	}
+}
