57f73d113303606cb358583c5e094c48569dce8b
Stufingo <geniekort@hotmail.com>
2016-10-13 10:45:54 +0200

    A

--- src/assignment2/Polynomial.java
+++ src/assignment2/Polynomial.java
@@ -1,5 +1,68 @@
 package assignment2;
 
+import java.util.ArrayList;
+
 public class Polynomial {
 
+	// Coefficients are ordered by significance, ie coefficients[0] = least significant coefficient
+	private ArrayList<IntegerMod> coefficients;
+	private int mod;
+	
+	/**
+	 * Constructs an empty polynomial
+	 * @param m 	The modulus
+	 */
+	public Polynomial(int m){
+		
+		mod = m;
+		coefficients = new ArrayList<IntegerMod>();
+		coefficients.add(new IntegerMod(0, m));
+	}
+	
+	/**
+	 * Constructs Polynomial from arraylist of coefficients
+	 * @param c 	Coefficients
+	 * @param m		Modulus
+	 */
+	public Polynomial(ArrayList<IntegerMod> c, int m){
+		
+		mod = m;
+		coefficients = c;
+	}
+	
+	/**
+	 * Produce the sum of the polynomials
+	 * @param x
+	 * @return
+	 */
+	public Polynomial sum(Polynomial x){
+		return null;
+	}
+	
+	/**
+	 * Produce the scalar multiple
+	 * @param x
+	 * @return
+	 */
+	public Polynomial scalarMultiple(int x){
+		return null;
+	}
+	
+	/**
+	 * Returns the difference with x
+	 * @param x
+	 * @return
+	 */
+	public Polynomial difference(Polynomial x){
+		return null;
+	}
+	
+	/**
+	 * Returns the product with x
+	 * @param x
+	 * @return
+	 */
+	public Polynomial product(Polynomial x){
+		return null;
+	}
 }
