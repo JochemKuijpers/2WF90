e8124625649eac8bb32e5becef9d6398c442a9a7
Stufingo <geniekort@hotmail.com>
2016-10-24 15:48:59 +0200

    Because Mircea did not upload in the right spot

--- src/assignment2/Polynomial.java
+++ src/assignment2/Polynomial.java
@@ -97,8 +97,34 @@ public class Polynomial {
 	 * @param x
 	 * @return
 	 */
+
+
 	public Polynomial sum(Polynomial x) {
-		return null;
+		ArrayList<IntegerMod> a, b, summ, zero;
+		a = this.coefficients;
+		b = x.coefficients;
+		summ = new ArrayList<IntegerMod>();
+		zero = ZERO(x.mod).coefficients;
+
+		if (a.size() <= b.size()){
+			for(int i = 0; i < a.size(); i++){
+				summ.add((a.get(i)).add(b.get(i)));
+			}
+			for(int i = a.size(); i < b.size(); i++){
+				summ.add((zero.get(0)).add(b.get(i)));
+			}
+		}
+
+		else {
+			for(int i = 0; i < b.size(); i++){
+				summ.add((a.get(i)).add(b.get(i)));
+			}
+			for(int i = b.size(); i < a.size(); i++){
+				summ.add((zero.get(0)).add(a.get(i)));
+			}
+		}
+
+		return new Polynomial(summ, x.mod);
 	}
 
 	/**
@@ -108,7 +134,17 @@ public class Polynomial {
 	 * @return
 	 */
 	public Polynomial scalarMultiple(int x) {
-		return null;
+		ArrayList<IntegerMod> a, mult, scal;
+		a = this.coefficients;
+		mult = new ArrayList<IntegerMod>();
+		scal = new ArrayList<IntegerMod>();
+		scal.add(new IntegerMod(x, this.mod));
+
+		for(int i = 0; i < a.size(); i++){
+			mult.add((scal.get(0)).multiply(a.get(i)));
+		}
+
+		return new Polynomial(mult, this.mod);
 	}
 
 	/**
@@ -118,7 +154,31 @@ public class Polynomial {
 	 * @return
 	 */
 	public Polynomial difference(Polynomial x) {
-		return null;
+		ArrayList<IntegerMod> a, b, diff, zero;
+		a = this.coefficients;
+		b = x.coefficients;
+		diff = new ArrayList<IntegerMod>();
+		zero = ZERO(x.mod).coefficients;
+
+		if (a.size() <= b.size()){
+			for(int i = 0; i < a.size(); i++){
+				diff.add((a.get(i)).subtract(b.get(i)));
+			}
+			for(int i = a.size(); i < b.size(); i++){
+				diff.add((zero.get(0)).subtract(b.get(i)));
+			}
+		}
+
+		else {
+			for(int i = 0; i < b.size(); i++){
+				diff.add((a.get(i)).subtract(b.get(i)));
+			}
+			for(int i = b.size(); i < a.size(); i++){
+				diff.add((zero.get(0)).subtract(a.get(i)));
+			}
+		}
+
+		return new Polynomial(diff, x.mod);
 	}
 
 	/**
@@ -128,7 +188,50 @@ public class Polynomial {
 	 * @return
 	 */
 	public Polynomial product(Polynomial x) {
-		return null;
+		ArrayList<IntegerMod> a, b, prod, zero;
+		a = this.coefficients;
+		b = x.coefficients;
+		prod = new ArrayList<IntegerMod>();
+		zero = ZERO(x.mod).coefficients;
+		IntegerMod coefj = zero.get(0);
+
+		if(a.size() <= b.size()){
+			for(int i = a.size(); i < 2 * b.size(); i++){
+				a.add(zero.get(0));
+			}
+			for(int i = b.size(); i < 2 * b.size(); i++){
+				b.add(zero.get(0));
+			}
+		}
+		else{
+			for(int i = a.size(); i < 2 * a.size(); i++){
+				a.add(zero.get(0));
+			}
+			for(int i = b.size(); i < 2 * a.size(); i++){
+				b.add(zero.get(0));
+			}
+		}
+
+		if(a.size() <= b.size()){
+			for(int i = 0; i < 2 * b.size(); i++){
+				coefj = zero.get(0);
+				for(int j = 0; j <= i; j++) {
+					coefj = coefj.add((a.get(j)).multiply(b.get(i - j)));
+				}
+				prod.add(coefj);
+			}
+		}
+		else{
+			for(int i = 0; i < 2 * a.size(); i++){
+				coefj = zero.get(0);
+				for(int j = 0; j <= i; j++) {
+					coefj = coefj.add((a.get(j)).multiply(b.get(i - j)));
+				}
+				prod.add(coefj);
+			}
+		}
+
+		return new Polynomial(prod, x.mod);
 	}
 	
 	/**
@@ -137,8 +240,23 @@ public class Polynomial {
 	 * @return		(quot,rem) such that (this == quot * b + rem)
 	 */
 	public Polynomial.DivisionResult divide(Polynomial b) {
+		Polynomial q, r, lcDiv;
+		q = ZERO(b.mod);
+		r = this;
 
-		return null;
+		while (r.coefficients.size() >= b.coefficients.size()){
+			int lcr, lcb;
+			lcr = r.coefficients.get(r.coefficients.size() - 1).getValue();
+			lcb = b.coefficients.get(b.coefficients.size() - 1).getValue();
+			ArrayList<IntegerMod> lc = new ArrayList<IntegerMod>();
+			lc.add(new IntegerMod(lcr / lcb, this.mod));
+			lcDiv = new Polynomial(lc, this.mod);
+
+			q = q.sum(lcDiv);
+			r = r.difference(lcDiv.product(b));
+		}
+
+		return new Polynomial.DivisionResult(q, r);
 	}
 	
 	/**
@@ -149,9 +267,29 @@ public class Polynomial {
 	 * @return		gcd, x and y such that this * x + other * y = gcd
 	 */
 	public Polynomial.ExtEuclideanResult gcd(Polynomial other) {
-		
-		
-		return null;
+		Polynomial a, b, x, y, xp, yp, u, v, q, zero;
+		a = this;
+		b = other;
+		zero = ZERO(this.mod);
+		x = ONE(this.mod);
+		v = ONE(this.mod);
+		y = zero;
+		u = zero;
+
+		while (b.coefficients.get(b.coefficients.size() - 1) != zero.coefficients.get(0)){
+			Polynomial.DivisionResult divi = a.divide(b);
+			q = divi.quot;
+			a = b;
+			b = divi.rem;
+			xp = x;
+			yp = y;
+			x = u;
+			y = v;
+			u = xp.difference(q.product(u));
+			v = yp.difference(q.product(v));
+		}
+
+		return new Polynomial.ExtEuclideanResult(a, x, y);
 	}
 	
 	/**
@@ -160,8 +298,20 @@ public class Polynomial {
 	 * @return equality of the input and this polynomial
 	 */
 	public boolean equals(Polynomial x){
-		
-		return false;
+		ArrayList<IntegerMod> a, b;
+		a = this.coefficients;
+		b = x.coefficients;
+
+		if (a.size() != b.size())
+			return false;
+		else{
+			for(int i = 0; i < a.size(); i++){
+				if (a.get(i).getValue() != b.get(i).getValue()){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
 	/**
@@ -175,4 +325,18 @@ public class Polynomial {
 		Polynomial one = new Polynomial(o, m);
 		return one;
 	}
+
+	/**
+	 * Used to get a 0 polynomial in an easy way.a
+	 * @param m
+	 * @return The 0 polynomial, with modulus m
+	 */
+
+	public static Polynomial ZERO(int m){
+		ArrayList<IntegerMod> o = new ArrayList<IntegerMod>();
+		o.add(new IntegerMod(0, m));
+		Polynomial zero = new Polynomial(o, m);
+		return zero;
+	}
+
 }
